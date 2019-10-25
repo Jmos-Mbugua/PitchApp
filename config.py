78ac3c2ad1fca9e4051eaa68818506885400b981
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-25 03:55:06 +0300

    Add google mail configuration settings

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:7363@localhost/pitchapp'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    pass   
+      
 
 
 
