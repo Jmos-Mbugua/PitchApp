2446b1b3c0d214bb094a0065bedbb6cb953f9502
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-25 03:57:47 +0300

    Add the index function returning index html file with pitches categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,21 +1,19 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main 
 from flask_login import login_required, current_user
-from ..models import User, Pitch, Upvote, Downvote
-from .forms import UpdateProfile, PitchForm
+from ..models import User, Pitch, Upvote, Downvote, Comment
+from .forms import UpdateProfile, PitchForm, CommentsForm
 from .. import db, photos
 
 # Views    
-@main.route('/', methods = ['GET', 'POST'])
+@main.route('/')
 def index():
-   
-    return render_template('index.html')
-        
-       
-    
-    title = 'The first one minute can change the way people think about you'
-    return render_template('index.html', title = title)
+    pitches = Pitch.query.all()
+    product = Pitch.query.filter_by(category = 'Product').all()
+    interview = Pitch.query.filter_by(category='Interview').all()
+    business = Pitch.query.filter_by(category='Business')
 
+    return render_template('index.html',product=product,business=business,interview=interview,pitches=pitches)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
@@ -105,3 +103,18 @@ def dislike(id):
     new_downvote = Downvote(user=current_user,pitch_id=id)
     new_downvote.save()
     return redirect(url_for('main.index',id=id))
+
+@main.route('/comment/<int:pitch_id>',methods=['GET','POST'])
+@login_required
+def comment(pitch_id):
+    form = CommentsForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_comments()
+        return redirect(url_for('.comment',pitch_id=pitch_id))
+    return render_template('comments.html',form=form,pitch=pitch,all_comments=all_comments)
